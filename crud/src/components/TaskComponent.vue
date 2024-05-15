56c2d5894046611c7fbd70b1436a58e4527f6f0a
nateos <sodjanathan@gmail.com>
2024-05-15 20:10:00 +0000

    passing props from hi to lower

---
+++ crud/src/components/TaskComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    
+    <TaskListComponent :task="task" key="task.id" />
+  </div>
+</template>
+
+<script>
+import TaskListComponent from "./TaskListComponent.vue";
+
+export default {
+  name: "TaskComponent",
+  components: {
+    TaskListComponent,
+  },
+  props: {
+    tasks: Array,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ crud/src/components/TaskListComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="task">
+    <h3>{{ task.title }}</h3>
+    <p>{{ task.description }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "TaskListComponent",
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+</style>
