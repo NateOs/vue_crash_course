743872d6cc62b6356887bd09a0232256bf344d06
nateos <sodjanathan@gmail.com>
2024-05-15 20:09:33 +0000

    importing a child component

--- crud/src/components/HeaderComponent.vue
+++ crud/src/components/HeaderComponent.vue
@@ -1,10 +1,15 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
+
+    <ButtonComponent text="Add Task" color="green" />
+    <!-- <ButtonComponent text="Update Task" color="blue" />
+    <ButtonComponent text="Delete Task" color="red" /> -->
   </header>
 </template>
 
 <script>
+import ButtonComponent from "./ButtonComponent.vue";
 export default {
   name: "HeaderComponent",
   props: {
@@ -13,6 +18,9 @@ export default {
       required: true,
       default: "Default Title",
     },
+  },
+  components: {
+    ButtonComponent,
   },
 };
 </script>
