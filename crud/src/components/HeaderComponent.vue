28449fadf1b382498887311b65d087ea114e01d1
nateos <sodjanathan@gmail.com>
2024-05-15 15:54:38 +0000

    basic setup with some styles

---
+++ crud/src/components/HeaderComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: "Default Title",
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
