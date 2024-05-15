4c49398821cb8683a16aa28a784f0957a28d2328
nateos <sodjanathan@gmail.com>
2024-05-15 20:08:58 +0000

    adding a method

---
+++ crud/src/components/ButtonComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <button :style="{ background: color }" class="btn" @click="handleClick">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ButtonComponent",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    handleClick() {
+      console.log("clicked");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
