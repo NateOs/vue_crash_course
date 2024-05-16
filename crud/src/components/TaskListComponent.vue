5b54bfcf8f4caf3a1a567072e32099fc5f260b9c
nateos <sodjanathan@gmail.com>
2024-05-16 09:49:25 +0000

    added font awesome

--- crud/src/components/TaskListComponent.vue
+++ crud/src/components/TaskListComponent.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="task">
-    <h3>{{ task.title }}</h3>
+    <div class="task-title">
+      <h3>
+        {{ task.title }}
+      </h3>
+      <i class="fas fa-times"></i>
+    </div>
+
     <p>{{ task.description }}</p>
   </div>
 </template>
@@ -17,10 +23,18 @@ export default {
 </script>
 
 <style scoped>
+.fas {
+  color: red;
+}
 .task {
   background: #f4f4f4;
   margin: 5px;
   padding: 10px 20px;
   cursor: pointer;
+}
+.task-title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
